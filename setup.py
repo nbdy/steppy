fce11874a35404ce310da6f04620910648612f7c
smthnspcl <pascal@eberlein.io>
2020-06-06 00:19:59 +0200

    update

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    long_description_content_type="text/markdown",
-    long_description=open("readme.md", "r").read(),
-    name="steppy",
-    version="0.42",
-    description="find jobs with your cli on stepstone",
+    long_description=open("README.rst", "r").read(),
+    name="shoppy",
+    version="0.1",
+    description="modular shop crawler",
     author="Pascal Eberlein",
     author_email="pascal@eberlein.io",
-    url="https://github.com/smthnspcl/steppy",
+    url="https://github.com/smthnspcl/shoppy",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-    keywords="stepstone python api job crawler",
+    keywords="modular shop product crawler",
     packages=find_packages(),
-    entry_points={'console_scripts': ['steppy = steppy.__main__:main']},
+    entry_points={'console_scripts': ['shoppy = shoppy.__main__:main']},
     install_requires=open("requirements.txt").readlines()
 )
